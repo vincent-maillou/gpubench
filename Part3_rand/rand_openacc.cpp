8e5c690b9984e31108e0069bda097e40cc442ad9
vincent-maillou <vincent.maillou0@gmail.com>
2022-07-13 17:01:49 +0200

    firsts ideas
    Signed-off-by: vincent-maillou <vincent.maillou0@gmail.com>

--- Part3_rand/rand_openacc.cpp
+++ Part3_rand/rand_openacc.cpp
@@ -7,7 +7,8 @@
 using namespace std ;
 
 
-/*
+/*  WORK IN PROGRESS..
+
   1. How to resize a vector on the GPU
     - #pragma acc shape[( shape-name-string )] shape-clause-list
 
@@ -15,6 +16,13 @@ using namespace std ;
   2. How to generate random number in parallel on the GPU
     - Using random number generator require to compile with: "nollvm"
     - -acc -ta=tesla:nollvm -Mcudalib=curand
+
+    cuRAND: https://docs.nvidia.com/cuda/curand/introduction.html#introduction
+    CPU-side generation: /include/curand.h
+    GPU-side generation: /include/curand_kernel.h
+
+    /opt/nvidia/hpc_sdk/Linux_x86_64/22.2/math_libs/include/curand_kernel.h
+    For exemples: /opt/nvidia/hpc_sdk/Linux_x86_64/22.2/examples/CUDA-Libraries/cuRAND
 */
 
 
@@ -65,6 +73,10 @@ int main(int argc,char**argv)
     cout << endl;
   }
 
+  double *restrict pX( &X[0] ); 
+  size_t tX( X.size() );
+  #pragma acc enter data copyin(pX[:tX])
+
 
 
   /* ---------modification of X vector--------- */
@@ -77,6 +89,7 @@ int main(int argc,char**argv)
       X.shrink_to_fit();
       X.reserve(marge*N);
 
+      // #pragma acc data present(pX[:tX])
       for(int i=0;i<X.size();i++){
         if(X[i]>limit)
           X.push_back(dist(mt));
@@ -90,10 +103,14 @@ int main(int argc,char**argv)
   cout << "   time: " << elapsed.count()/1000000000.0 << " [s]" << endl;
 
   if(lvlOut>0){
+    #pragma acc update self(pX[:tX])
     for(const auto & v : X)
       cout << v << " ";
     cout << endl;
   }
+
+  #pragma acc exit data delete(pX[:tX])
+
 
   cout << "completed." << endl;
   return 0 ;
